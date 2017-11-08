c4c0c5e65fbecaffead498d322a7f7afadedb1b9
Loris <loris4526@gmail.com>
2017-11-08 15:08:22 +0100

    Clean up 'ship_test.js' to setup 'player' obj just once before specs, instead of defining new one for each unit test

--- test/ship_test.js
+++ test/ship_test.js
@@ -5,51 +5,11 @@ describe('checkForShip()',function(){
 
   //import 'checkForShip' function to test
   const {checkForShip} = require('../game_logic/ship_methods')
+  let player
 
-  it(`should report no ship at a given player's coordinate`, function(){
-    //create dump obj player, assuming has a ship at [0,0]
-    const player = {
-      ships: [
-        {
-          locations: [[0,0]]
-        }
-      ]
-    }
-
-    assert.isFalse(checkForShip(player,[9,9]))
-  })
-
-
-  it(`should report a ship located at given player's coordinate`, function(){
-    const player = {
-      ships: [
-        {
-          locations: [[0,0],[0,1]]
-        }
-      ]
-    }
-
-    assert.deepEqual(checkForShip(player,[0,0]), player.ships[0])
-  })
-
-
-  it(`should handle ships located at more than one coordinate`, function(){
-    const player = {
-      ships: [
-        {
-          locations: [[0,0], [1,0],[2,0]]
-        }
-      ]
-    }
-
-    assert.isFalse(checkForShip(player,[2,2]))
-    assert.deepEqual(checkForShip(player,[2,0]), player.ships[0])
-    assert.deepEqual(checkForShip(player,[1,0]), player.ships[0])
-  })
-
-
-  it(`should handle checking multiple ships`, function(){
-    const player = {
+  //this function will run before any of the test specs, to setup any state they need
+  before(function(){
+    player = {
       ships: [
         {
           locations: [[0,0], [1,0],[2,0]]
@@ -59,10 +19,29 @@ describe('checkForShip()',function(){
         },
         {
           locations: [[7,1], [7,2]]
-        },
+        }
       ]
     }
+  })
 
+  it(`should report no ship at a given player's coordinate`, function(){
+    assert.isFalse(checkForShip(player,[9,9]))
+  })
+
+
+  it(`should report a ship located at given player's coordinate`, function(){
+    assert.deepEqual(checkForShip(player,[0,0]), player.ships[0])
+  })
+
+
+  it(`should handle ships located at more than one coordinate`, function(){
+    assert.isFalse(checkForShip(player,[2,2]))
+    assert.deepEqual(checkForShip(player,[2,0]), player.ships[0])
+    assert.deepEqual(checkForShip(player,[1,0]), player.ships[0])
+  })
+
+
+  it(`should handle checking multiple ships`, function(){
     assert.isFalse(checkForShip(player,[2,2]))
     assert.deepEqual(checkForShip(player,[2,0]), player.ships[0])
     assert.deepEqual(checkForShip(player,[5,3]), player.ships[1])
