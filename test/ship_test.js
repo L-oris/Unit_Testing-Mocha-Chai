f94e9a1181a6e6444ca07c1845a877cb68de476b
Loris <loris4526@gmail.com>
2017-11-08 15:45:44 +0100

    Refactor 'fireShip()' test suite

--- test/ship_test.js
+++ test/ship_test.js
@@ -7,7 +7,6 @@ describe('checkForShip()',function(){
   const {checkForShip} = require('../game_logic/ship_methods')
   let player
 
-  //this function will run before any of the test specs, to setup any state they need
   before(function(){
     player = {
       ships: [
@@ -25,16 +24,19 @@ describe('checkForShip()',function(){
   })
 
   it(`should report no ship at a given player's coordinate`, function(){
+
     assert.isFalse(checkForShip(player,[9,9]))
   })
 
 
   it(`should report a ship located at given player's coordinate`, function(){
+
     assert.deepEqual(checkForShip(player,[0,0]), player.ships[0])
   })
 
 
   it(`should handle ships located at more than one coordinate`, function(){
+
     assert.isFalse(checkForShip(player,[2,2]))
     assert.deepEqual(checkForShip(player,[2,0]), player.ships[0])
     assert.deepEqual(checkForShip(player,[1,0]), player.ships[0])
@@ -42,6 +44,7 @@ describe('checkForShip()',function(){
 
 
   it(`should handle checking multiple ships`, function(){
+
     assert.isFalse(checkForShip(player,[2,2]))
     assert.deepEqual(checkForShip(player,[2,0]), player.ships[0])
     assert.deepEqual(checkForShip(player,[5,3]), player.ships[1])
@@ -58,6 +61,7 @@ describe('damageShip()',function(){
   const {damageShip} = require('../game_logic/ship_methods')
 
   it(`should register damage on a given ship at a given location`, function(){
+
     const ship = {
       locations: [[0,0]],
       damage: []
@@ -75,9 +79,10 @@ describe('damageShip()',function(){
 describe('fireShip()',function(){
 
   const {fireShip} = require('../game_logic/ship_methods')
+  let player
 
-  it(`should record damage on given player ship at given coordinate`, function(){
-    const player = {
+  beforeEach(function(){
+    player = {
       ships: [
         {
           locations: [[0,0]],
@@ -85,22 +90,17 @@ describe('fireShip()',function(){
         }
       ]
     }
-    fireShip(player,[0,0])
+  })
 
+  it(`should record damage on given player ship at given coordinate`, function(){
+
+    fireShip(player,[0,0])
     assert.deepEqual(player.ships[0].damage[0], [0,0])
   })
 
   it(`should NOT record damage if there's no ship at given coordinates`, function(){
-    const player = {
-      ships: [
-        {
-          locations: [[0,0]],
-          damage: []
-        }
-      ]
-    }
-    fireShip(player,[9,9])
 
+    fireShip(player,[9,9])
     assert.isEmpty(player.ships[0].damage)
   })
 
